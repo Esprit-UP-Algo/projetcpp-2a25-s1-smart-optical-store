d66fdc26f55db4649eefe87d0fe6185ca0b87246
siwar-dot <khelfasiwar6@gmail.com>
2025-12-02 18:58:24 +0100

    integration

--- fournisseurwindow.cpp
+++ fournisseurwindow.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:868438e167c3c6444fc8fc912b13ed533fcb56bd9e749b14cf57162f315c21d8
-size 37937
+oid sha256:e51cd38e8f6e102c907627d52924b3154074e07ba6d80ecead99cbfb804edff9
+size 37859
